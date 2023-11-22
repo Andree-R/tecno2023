c02b6d6943eb313d381f61b660e143f762d20a0b
Masher1C <mafer456.ch@gmail.com>
2023-11-21 19:37:31 -0500

    MODAL LOGIN

--- server/vistas/home.php
+++ server/vistas/home.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1><?=$titulo?></h1>
-    <h3>bienvenido <?=$usuario?></h3>
-    
+    <h1><?= $titulo ?></h1>
+    <h3>bienvenido <?= $usuario ?></h3>
+
     <h3>Opciones:</h3>
     <ul>
-        <?php 
+        <?php
         foreach ($menu as $key => $value) {
         ?>
-        <li>
-            <a href="?ctrl=<?=$key?>">
-                <?=$value?>
-            </a>
-        </li>
+            <li>
+                <a href="?ctrl=<?= $key ?>">
+                    <?= $value ?>
+                </a>
+            </li>
 
         <?php
         }
@@ -27,6 +29,9 @@
 
     </ul>
 
+    
+
 
 </body>
+
 </html>
--- server/vistas/personas/login.php
+++ server/vistas/personas/login.php
@@ -1,3 +1,70 @@
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Launch demo modal
+</button>
+
+<div class="modal fade card card-outline card-primary" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+    <div class="card-header text-center">
+      <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
+    </div>
+      <div class="modal-body">
+        <div class="card-body">
+          <p class="login-box-msg">Regístrate para iniciar tu sesión</p>
+          <form action="?ctrl=CtrlPersona&accion=validar" method="post">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" name="usuario" placeholder="Usuario">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-user"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="password" class="form-control" name="clave" placeholder="Contraseña">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-8">
+                <div class="icheck-primary">
+                  <input type="checkbox" id="remember">
+                  <label for="remember">
+                    Recordarme
+                  </label>
+                </div>
+              </div>
+
+              <div class="col-4">
+                <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
+              </div>
+
+            </div>
+          </form>
+          <div class="social-auth-links text-center mt-2 mb-3">
+            <a href="#" class="btn btn-block btn-primary">
+              <i class="fab fa-facebook mr-2"></i> Ingresar usando Facebook
+            </a>
+            <a href="#" class="btn btn-block btn-danger">
+              <i class="fab fa-google mr-2"></i> Ingresar usando Google
+            </a>
+          </div>
+
+          <p class="mb-1 accent-lightblue">
+            <a href="forgot-password.html">I forgot my password</a>
+          </p>
+          <p class="mb-0 accent-lightblue">
+            <a href="register.html" class="text-center">Register a new membership</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 <div class="login-box">
 
   <div class="card card-outline card-primary">
